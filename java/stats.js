1e054e19176f460a5d5e07139d0ae917ca01da8d
emilio45214 <emilianobrizio2017@gmail.com>
2023-01-13 15:07:21 -0300

    detalless

--- java/stats.js
+++ java/stats.js
@@ -1,14 +1,12 @@
 const {createApp} = Vue
 
 createApp({
-    data(){ // las variables que puedo usar en el html
+    data(){ 
       return {
-
         data: undefined,
         upcomingFiltered: undefined,
         pastFiltered: undefined,
         maxMinPercentage: [],
-
       }  
     },
     created(){
@@ -24,13 +22,11 @@ createApp({
                 this.maxPercentage(listaPorcentaje)
                 this.minPercentage(listaPorcentaje)
                 console.log(this.maxMinPercentage)
-                
-                
             })
             .catch(err => console.log(err))
     },
     methods:{
-        revenues : function (prices, estimatesOrAssistance){ //reemplazar por data.(price o estimate o assistance)
+        revenues : function (prices, estimatesOrAssistance){ 
             let rev = prices * estimatesOrAssistance
             return rev.toLocaleString()
     },
@@ -41,25 +37,16 @@ createApp({
         newPropertyPercentage : function (data){
         let list = []
         let filteredAssistance = data.events.filter( event => event.assistance)
-        
             for (let i = 0; i < filteredAssistance.length; i++) {
                     list.push(filteredAssistance[i]);
-                    
-                
-                
                     list[i].percentage = this.percentageOfAttendance(list[i].capacity, list[i].assistance);
-                
             }
-            
             return [...list.sort((event1, event2) => event2.percentage - event1.percentage)]
         },
         maxPercentage : function (events2){
             let sortedMax = [...events2.sort((event1, event2) => event2.percentage - event1.percentage)]
             this.maxMinPercentage[0] = {name: sortedMax[0].name + " with " , percentage: sortedMax[0].percentage +"%"}
-            
-            
         },
-        
         minPercentage : function (events2){
             let sortedMin = [...events2.sort((event1, event2) => event1.percentage - event2.percentage)]
             this.maxMinPercentage[1] = {name: sortedMin[0].name + " with ", percentage: sortedMin[0].percentage + "%"}
@@ -68,8 +55,6 @@ createApp({
             let maximCapacity = events.sort((event1, event2) => event2.capacity - event1.capacity)
             this.maxMinPercentage[2] = {name: maximCapacity[0].name + " with ", capacity: (maximCapacity[0].capacity).toLocaleString() + " of capacity."} 
         }
-
-        
     }
 }).mount("#APP")
 
